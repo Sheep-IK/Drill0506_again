882bd449d589d86221fb8a1a8072097285552398
Sheep-IK-LT <yik418@naver.com>
2023-10-04 18:06:38 +0900

    reformat

--- Drill06.py
+++ Drill06.py
@@ -21,9 +21,9 @@ def handle_events():
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            mx,my = event.x, TUK_HEIGHT -1 -event.y
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            points.append((event.x, TUK_HEIGHT - 1 - event.y)) #클릭된 위치를 새로운 점으로 추가.
+            points.append((event.x, TUK_HEIGHT - 1 - event.y))  # 클릭된 위치를 새로운 점으로 추가.
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -36,7 +36,7 @@ def reset_world():
     global mx, my
     global points
 
-    mx, my = 0,0
+    mx, my = 0, 0
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
@@ -52,18 +52,20 @@ def set_new_target_arrow():
     global frame
     global target_exists
 
-    if points: #points 리스트에 남아있는 점이 있으면,
+    if points:  # points 리스트에 남아있는 점이 있으면,
         sx, sy = cx, cy  # p1 : 시작점
-        #hx, hy = TUK_WIDTH-100, TUK_HEIGHT-100
+        # hx, hy = TUK_WIDTH-100, TUK_HEIGHT-100
         hx, hy = points[0]  # p2 : 끝점
         t = 0.0
         action = 1 if sx < hx else 0
         frame = 0
         target_exists = True
     else:
-        action = 3 if action == 1 else 2 #이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
+        action = 3 if action == 1 else 2  # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
         frame = 0
         target_exists = False
+
+
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -86,13 +88,12 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx 는 시작 x와 끝 x를 1-t:t 의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else:   #목표지점에 도달하면
-            cs, cy = hx, hy #캐릭터의 위치를 목적지까지 강제로 정확히 일치시킴.
-            del points[0] #목표지점에 왔기 때문에, 더이상 필요없는 점을 삭제
+        else:  # 목표지점에 도달하면
+            cs, cy = hx, hy  # 캐릭터의 위치를 목적지까지 강제로 정확히 일치시킴.
+            del points[0]  # 목표지점에 왔기 때문에, 더이상 필요없는 점을 삭제
             set_new_target_arrow()
-    elif points:  #목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
+    elif points:  # 목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
         set_new_target_arrow()
-
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
